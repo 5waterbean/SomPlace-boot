09deba9e76ef70df80efb4fce892d27db745d742
user <user@DESKTOP-4OALGTT>
2023-05-27 12:30:14 +0900

    [UPDATE] Irregular 관련 controller, dao, mapper, xml 등등

---
+++ src/main/java/com/somplace/dao/mybatis/mapper/IrregularMapper.java
@@ -0,0 +1,27 @@
+package com.somplace.dao.mybatis.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.somplace.domain.Irregular;
+import com.somplace.domain.Meeting;
+
+@Mapper
+public interface IrregularMapper {
+	//일시적모임 생성
+	void createIrregular(Irregular irregular);
+	//일시적모임 수정
+	void updateIrregular(Irregular irregular);
+	
+	//일시적모임 가져오기
+	Irregular getIrregularById(int meetingId);
+	
+	//일시적모임(키워드) 검색
+	List<Meeting> searchIrregularByKey(String key, String category);
+	
+	//일시적모임 인기순 정렬
+	List<Meeting> sortIrregularByHeart(String category);
+	//일시적모임 최신순 정렬
+	List<Meeting> sortIrregularByOrder(String category);
+}
